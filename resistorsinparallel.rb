9764ee2e74a9d14d5947ce86d99316a118084665
yuki0920 <ywsep20@gmail.com>
2019-08-19 07:20:55 +0900

    refactoring

--- resistorsinparallel.rb
+++ resistorsinparallel.rb
@@ -1,6 +1,7 @@
 n = gets.to_i
-a = gets.strip.split(" ").map(&:to_f)
-m = 1
-a.each{|a| m = a * m }
-l = a.map{ |a| m / a }.inject(:+)
-print (m / l)
+as = gets.strip.split(" ").map(&:to_f)
+m = 0
+as.each do |a|
+    m += 1/a
+end
+print 1/m
