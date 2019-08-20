7ff4ef34af0a7713979e5a500fe0379fbf5d8e38
yuki0920 <ywsep20@gmail.com>
2019-08-20 22:06:23 +0900

    refactoring

--- biteeating.rb
+++ biteeating.rb
@@ -1,6 +1,6 @@
 n,l = gets.split(" ").map(&:to_i)
 apples = (0...n).map{ |a| a + l }
-taste = apples.inject(:+)
+taste = (apples[0] + apples[n-1]) * n / 2
 bite = if apples.include?(0)
         0
        elsif taste > 0
