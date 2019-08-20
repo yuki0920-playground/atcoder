f20c60a4f60b761ca6cc2b62b84e3ee9e791b596
yuki0920 <ywsep20@gmail.com>
2019-08-20 19:17:05 +0900

    correct

--- gooddistance.rb
+++ gooddistance.rb
@@ -1,22 +1,13 @@
 n,d = gets.split(" ").map(&:to_i)
-x1 = gets.split(" ").map(&:to_i)
-x2 = gets.split(" ").map(&:to_i)
-x3 = gets.split(" ").map(&:to_i)
-
-answer1 = 0
-(0..(d-1)).each do |e|
-    answer1 += (x1[e] - x2[e]) ** 2
+points = []
+n.times{ points << gets.split.map(&:to_i) }
+count = 0
+points.combination(2) do |y,z|
+    dist = 0
+        (0...d).each do |e|
+        dist += (y[e] - z[e]) ** 2
+    end
+    dist = Math.sqrt(dist)
+    count += 1 if dist - dist.to_i == 0
 end
-print answer1
-
-answer2 = 0
-(0..(d-1)).each do |e|
-    answer2 += (x2[e] - x3[e]) ** 2
-end
-print answer2
-
-answer3 = 0
-(0..(d-1)).each do |e|
-    answer3 += (x3[e] - x1[e]) ** 2
-end
-print answer3
+print count
