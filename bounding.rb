2f666d2ffe663ef0880352e9e139fafcc63cfb5e
yuki0920 <ywsep20@gmail.com>
2019-08-21 08:56:54 +0900

    refactoring

--- bounding.rb
+++ bounding.rb
@@ -1,11 +1,12 @@
 n,x = gets.split(" ").map(&:to_i)
-length = gets.split(" ").map(&:to_i)
-# ball = [0]
-# a = 0
-# ball = l.map{ |l| l += a }
-a = 0
-bounds = length.map do |l|
-    a += l
-    l = a
+l = gets.split(" ").map(&:to_i)
+count = 1
+d = 0
+(0...n).each do |i|
+    d += l[i]
+    if d > x
+        break
+    end
+    count += 1
 end
-print bounds.count{ |bound| bound <= x } + 1
+print count
