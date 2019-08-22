2db52a880ba62a829feea8af526ca1c435cf25af
yuki0920 <ywsep20@gmail.com>
2019-08-23 08:26:30 +0900

    correct

--- yymmormmyy.rb
+++ yymmormmyy.rb
@@ -4,9 +4,9 @@ s2 = s[2..3].to_i
 
 if s1.between?(1,12) && s2.between?(1,12)
     print "AMBIGUOUS"
-elsif s1 != 0 && s2.between?(1,12)
+elsif s2.between?(1,12)
     print "YYMM"
-elsif s1.between?(1,12) && s2 != 0
+elsif s1.between?(1,12)
     print "MMYY"
 else
     print "NA"
