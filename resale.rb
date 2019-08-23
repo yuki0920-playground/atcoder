b19cf3d2233c033522632b8bfccc6a8217b02131
yuki0920 <ywsep20@gmail.com>
2019-08-23 19:01:12 +0900

    correct

--- resale.rb
+++ resale.rb
@@ -1,6 +1,9 @@
 n = gets.to_i
 v = gets.split(" ").map(&:to_i)
 c = gets.split(" ").map(&:to_i)
-profits = [v,c].transpose.map{ |n| n.inject(:-) }
-               .select{ |profit| profit > 0 }.inject(:+)
+profits = 0
+(1..n).each do |i|
+    profit = v[i-1] - c[i-1]
+    profits += profit if profit > 0
+end
 print profits
