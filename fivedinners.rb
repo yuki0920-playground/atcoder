f3388260a44d5ce2f21809d1bab453348dd36ca7
yuki0920 <ywsep20@gmail.com>
2019-08-26 23:45:08 +0900

    refactoring

--- fivedinners.rb
+++ fivedinners.rb
@@ -1,4 +1,4 @@
 dishes = []
 5.times{ dishes << gets.to_i }
-wait = dishes.map{ |dish| dish % 10 == 0 ? 0 : 10 - dish % 10  }
-print dishes.inject(:+) + wait.inject(:+) - wait.max
+wait = dishes.map{ |dish| dish % 10 == 0 ? 0 : 10 - dish % 10 }
+print dishes.map{ |n| n.ceil(-1) }.inject(:+) - wait.max
