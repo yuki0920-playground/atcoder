853d6bf2c1706b472429153ec87c468bac245f97
yuki0920 <ywsep20@gmail.com>
2019-09-03 07:35:30 +0900

    refactoring

--- canyousolvethis.rb
+++ canyousolvethis.rb
@@ -1,13 +1,12 @@
 N,M,C = gets.split(" ").map(&:to_i)
 b = gets.split(" ").map(&:to_i)
-A = []
-N.times{ A << gets.split(" ").map(&:to_i) }
 
 count = 0
-(0...N).each do |n|
+N.times do
+    a = gets.split(" ").map(&:to_i)
     sum = 0
-    A[n].each_with_index do |a, i|
-        sum += a * b[i]
+    M.times do |i|
+        sum += a[i] * b[i]
     end
     count += 1 if sum + C > 0
 end
