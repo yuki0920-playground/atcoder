c21f76b9a51cd6835688e2b017a3b43af85d3adb
yuki0920 <ywsep20@gmail.com>
2019-09-12 20:34:48 +0900

    refactoring

--- polygon.rb
+++ polygon.rb
@@ -1,7 +1,7 @@
-n = gets.split(" ").map(&:to_i)
+n = gets
 ls = gets.split(" ").map(&:to_i).sort
 
-max_length = ls.pop
-sum_lentghs = ls.inject(:+)
+max_length = ls.lash
+sum_lentghs = ls[0..-2].inject(:+)
 
 print max_length < sum_lentghs ? "Yes" : "No"
