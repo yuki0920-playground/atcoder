b3c3660e57e5dba396f53a6fee16e56b77e9cc84
yuki0920 <ywsep20@gmail.com>
2020-10-09 22:12:55 +0900

    refactoring

--- abc178/b.rb
+++ abc178/b.rb
@@ -1,11 +1,5 @@
 a, b, c, d = gets.chomp.split(' ').map(&:to_i)
 
-number =
-  [
-    [a, b].max * [c, d].max,
-    [a, b].max * [c, d].min,
-    [a, b].min * [c, d].max,
-    [a, b].min * [c, d].min
-  ].max
+number = [a * c, a * d, b * c, b * d].max
 
 puts number
