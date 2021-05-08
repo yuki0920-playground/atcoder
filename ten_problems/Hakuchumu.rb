ea34ae1f97c67b741df0f627672bf1267da27a20
yuki0920 <ywsep20@gmail.com>
2021-05-08 20:57:55 +0900

    Add Hakuchumu

--- ten_problems/Hakuchumu.rb
+++ ten_problems/Hakuchumu.rb
@@ -1,32 +1,19 @@
 str = gets.chomp
+length = str.length
 
-def extract_str(str)
-  # puts str
-
-  if str == ''
-    puts 'YES'
-    exit
+while str.length >= 5
+  %w(eraser erase dreamer dream).each do |word|
+    str.delete_suffix! word
   end
 
-  if str.length <= 4
+  if str.length == length
     puts 'NO'
     exit
   end
-
-  if str[-5..-1] == 'dream' || str[-5..-1] == 'erase'
-    extract_str(str[..-6])
-  end
-
-  if str[-6..-1] == 'eraser'
-    extract_str(str[..-7])
-  end
-
-  if str[-7..-1] == 'dreamer'
-    extract_str(str[..-8])
-  end
-
-  puts 'NO'
-  exit
 end
 
-extract_str(str)
+if str.length == 0
+  puts 'YES'
+else
+  puts 'NO'
+end
