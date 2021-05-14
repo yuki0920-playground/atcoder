88ab86e189bc38e7cdac68257e44136dd96577b3
yuki0920 <ywsep20@gmail.com>
2021-05-15 07:56:50 +0900

    Add AllGreen

--- AllGreen.rb
+++ AllGreen.rb
@@ -1,31 +1,33 @@
-require 'pry'
-d, g = gets.chomp.split(' ').map(&:to_i)
-problem_set = (1..d).map{ gets.chomp.split(' ').map(&:to_i) }
-count = 0
-[0, 1].repeated_permutation(d).each_with_index do |bits, i|
-  total_score = 0
-  total_ptoblems_count = 0
-  problem_set.each_with_index do |(problems_count, bonus), j|
-    if bits[j] == 1
-      total_score += 100 * (j+1) * problems_count + bonus
-      total_ptoblems_count += problems_count
-    end
+D, G = gets.split.map(&:to_i)
+problem_sets = Array.new(D) { gets.split.map(&:to_i) }
 
-    if total_score >= g && total_ptoblems_count <= count
-      count = total_ptoblems_count
-      next
-    end
+ans = Float::INFINITY
+# 全部解く難易度をbit全探索
+[0, 1].repeated_permutation(D).each do |bits|
+  score = 0
+  problems = 0
+  problem_sets.each_with_index do |(each_count, bonus), i|
+    next if bits[i] == 0
+
+    score += 100 * (i + 1) * each_count + bonus
+    problems += each_count
   end
 
-  problem_set.each_with_index.reverse_each do |(problems_count, bonus), j|
-    point = problem_set.count - j + 1
-    if bits.reverse[j] == 0
-      (1..problems_count).each do
-        total_score += 100 * point
-        total_ptoblems_count += 1
-      end
-    end
+  # reverse_eachにインデックスを付けるとインデックスも逆順となる
+  problem_sets.each_with_index.reverse_each do |(each_count, _), i|
+    break if score >= G
+    next if bits[i] == 1
+
+    s = (i + 1) * 100
+    n = ((G - score) / s).ceil
+
+    break if n > each_count
+
+    score += s * n
+    problems += n
   end
+
+  ans = [ans, problems].min if score >= G
 end
 
-puts count
+puts ans
