3b452d2a3718c173542d57bb1f38456dcdf54d8f
yuki0920 <ywsep20@gmail.com>
2021-05-19 09:36:09 +0900

    Update

--- dp/Frog2.rb
+++ dp/Frog2.rb
@@ -1,25 +1,29 @@
 N, K = gets.chomp.split.map(&:to_i)
 h = gets.chomp.split.map(&:to_i)
 
-# 足場i+1個目までに支払う支払う最小コスト
-DP = Hash.new(10001)
+# 足場i+1番目までに支払う支払う最小コスト
+DP = {}
 
 # 漸化式
-# DP[i] = [DP[i], DP[i - j] + [h[i] - h[j]].abs].min
-# ただし if i < j
+# ただし if i + j >= N
+# DP[i + j] = DP[i] + (h[i + j] - h[i]).abs
+# DP[i + j] = [DP[i + j], DP[i] + (h[i + j] - h[i]).abs].min ただし DP[i+j]があるとき
+
 
 # 初期化
 DP[0] = 0
 
 (0...N).each do |i|
   (1..K).each do |j|
-    # hのインデックスは最大でもN - 1までなので
-    next if j > N - 1
-    # i+jのインデックスは最大でもNまでなので
-    next if i + j > N
+    # i+jのインデックスは最大でもN-1までなので
+    next if i + j >= N
 
-    DP[i + j] = [DP[i + j].to_i, DP[i].to_i + (h[i] - h[j]).abs].min
+    unless DP[i + j]
+      DP[i + j] = DP[i] + (h[i + j] - h[i]).abs
+    end
+
+    DP[i + j] = [DP[i + j], DP[i] + (h[i + j] - h[i]).abs].min
   end
 end
 
-puts DP[N]
+puts DP[N - 1]
